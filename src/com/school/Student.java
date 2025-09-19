f658138f664695340176d1fa2427cec6a52945f2
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-09-19 11:39:41 +0530

    feat: Implement inheritance hierarchy for Person, Student, Teacher, Staff (Part 5)

--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -1,24 +1,20 @@
 package com.school;
 
-public class Student {
-    private static int nextStudentIdCounter = 1;
-    private int studentId;
-    private String name;
+public class Student extends Person {
+    private String gradeLevel;
 
-    public Student(String name) {
-        this.studentId = nextStudentIdCounter++;
-        this.name = name;
+    public Student(String name, String gradeLevel) {
+        super(name);
+        this.gradeLevel = gradeLevel;
     }
 
-    public int getStudentId() {
-        return studentId;
+    public String getGradeLevel() {
+        return gradeLevel;
     }
 
-    public String getName() {
-        return name;
-    }
-
+    @Override
     public void displayDetails() {
-        System.out.println("Student ID: " + this.studentId + ", Name: " + this.name);
+        super.displayDetails();
+        System.out.println("Grade Level: " + gradeLevel + ", Role: Student");
     }
 }
