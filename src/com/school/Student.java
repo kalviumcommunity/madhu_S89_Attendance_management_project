ec921df204e3e2996bfc2fe0a12f8f331054e9b7
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-09-20 09:46:43 +0530

    feat: Implement Storable interface and FileStorageService for saving data (Part 6)

--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -1,6 +1,6 @@
 package com.school;
 
-public class Student extends Person {
+public class Student extends Person implements Storable {
     private String gradeLevel;
 
     public Student(String name, String gradeLevel) {
@@ -16,5 +16,10 @@ public class Student extends Person {
     public void displayDetails() {
         super.displayDetails();
         System.out.println("Grade Level: " + gradeLevel + ", Role: Student");
+    }
+
+    @Override
+    public String toDataString() {
+        return getId() + "," + getName() + "," + gradeLevel;
     }
 }
